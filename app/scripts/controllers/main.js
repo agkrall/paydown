7395ee65b3382353d6401d3d074a7ad546c825d8
Andrew Krall <akrall0820@gmail.com>
2015-05-08 15:27:59 -0500

    project from yeoman angular generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name paydownApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the paydownApp
+ */
+angular.module('paydownApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
